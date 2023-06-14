3ab06ffd2e40b5c64b1604796f04aba1d495bc14
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-06-14 14:25:31 -0300

    chore: create PokemonList page skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,7 @@
-import { useEffect, useState } from 'react';
-
-type Pokemon = {
-  id: number;
-  name: string;
-  sprite: string;
-};
-
-type FetchPokemon = (id: number, pokemons: Pokemon[]) => Promise<Pokemon[]>;
-
-type FetchManyPokemons = (amount: number) => Promise<Pokemon[]>;
-
-const fetchManyPokemons: FetchManyPokemons = (amount: number) => {
-  const result = [];
-
-  for (const index of Array(amount).keys()) {
-    result.push((result: Pokemon[]) => fetchPokemon(index + 1, result));
-  }
-
-  return result.reduce((p, f) => p.then(f), Promise.resolve([] as Pokemon[]));
-};
-
-const fetchPokemon: FetchPokemon = async (id, pokemons) => {
-  if (pokemons.find((p) => p.id === id)) {
-    return Promise.resolve(pokemons);
-  }
-
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-  const data = await response.json();
-
-  const pokemon: Pokemon = {
-    id: data.id,
-    name: data.name,
-    sprite: data.sprites.front_default,
-  };
-
-  return [...pokemons, pokemon];
-};
+import { PokemonList } from './pages/PokemonList';
 
 function App() {
-  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
-
-  useEffect(() => {
-    fetchManyPokemons(10).then((result) => setPokemon(result));
-  }, []);
-
-  return (
-    <>
-      {pokemon.map((pokemon) => {
-        return (
-          <div key={pokemon.id}>
-            <img src={pokemon.sprite} alt={pokemon.name} />
-            <p>{pokemon.name}</p>
-          </div>
-        );
-      })}
-    </>
-  );
+  return <PokemonList />;
 }
 
 export default App;

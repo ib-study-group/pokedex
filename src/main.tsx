5e3614209b6a7393b6db191418cbd70655321edd
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-06-14 14:40:50 -0300

    chore: add custom fonts

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 
+import '@fontsource/poppins';
 import './styles/reset.css';
+import './styles/theme.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
